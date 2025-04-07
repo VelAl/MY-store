c1ab5de7288873939df78c950ad530bbeef160ad
Alex <welmozhko88@gmail.com>
2025-04-07 09:40:28 +0300

    added Separator component

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,3 +1,16 @@
-export const SectionTitle = () => {
-  return <div>SectionTitle</div>;
+import { FC } from "react";
+
+import { Separator } from "./ui/separator";
+
+type T_Props = { text: string };
+
+export const SectionTitle: FC<T_Props> = ({ text }) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-medium tracking-wider capitalize mb-8">
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
 };
