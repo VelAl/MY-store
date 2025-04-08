62efca7507a5909c537f76d7890035ba085339cb
Alex <welmozhko88@gmail.com>
2025-04-08 09:08:45 +0300

    added FormSelect component

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,11 +1,20 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import { Form, Link, useLoaderData, useSearchParams } from "react-router-dom";
+
+import { T_ProductsResponse } from "@/utils";
 
 import { FormInput } from "./FormInput";
+import { FormSelect } from "./FormSelect";
 import { Button } from "./ui/button";
 
+const sortOptions = ["a-z", "z-a", "high", "low"];
+
 export const Filters = () => {
+  const { meta } = useLoaderData() as T_ProductsResponse;
   const [searchParams] = useSearchParams();
   const search = searchParams.get("search") || "";
+  const category = searchParams.get("category") || undefined;
+  const company = searchParams.get("company") || undefined;
+  const order = searchParams.get("order") || undefined;
 
   return (
     <Form
@@ -13,15 +22,33 @@ export const Filters = () => {
                  lg:grid-cols-4 items-center"
     >
       <FormInput label="Search product" name="search" defaultValue={search} />
+      <FormSelect
+        name="category"
+        label="Select category"
+        defaultValue={category}
+        options={meta.categories}
+      />
+      <FormSelect
+        name="company"
+        label="Select company"
+        defaultValue={company}
+        options={meta.companies}
+      />
+      <FormSelect
+        name="order"
+        label="Sort by"
+        defaultValue={order}
+        options={sortOptions}
+      />
       <Button type="submit" size="sm" className="self-end mb-2">
-        Search
+        Apply filters
       </Button>
       <Button
         type="button"
         asChild
         size="sm"
-        variant="outline"
-        className="self-end mb-2"
+        variant="secondary"
+        className="self-end mb-2 border"
       >
         <Link to="/products">reset</Link>
       </Button>
---
+++ src/components/FormSelect.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+
+import { Label } from "./ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+type T_SelectProps = {
+  name: string;
+  label?: string;
+  defaultValue?: string;
+  options: string[];
+};
+
+export const FormSelect: FC<T_SelectProps> = ({
+  name,
+  options,
+  defaultValue,
+  label,
+}) => {
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize mb-1">
+        {label || name}
+      </Label>
+      <Select name={name} defaultValue={defaultValue || options[0]}>
+        <SelectTrigger id={name} className="w-full">
+          <SelectValue />
+        </SelectTrigger>
+
+        <SelectContent>
+          {options.map((option) => (
+            <SelectItem key={option} value={option}>
+              {option}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
